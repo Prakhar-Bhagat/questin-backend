9588a1b9d5e5bfd7fe70333dc04dc660dfb2f068
Prakhar Bhagat <Prakhar.Bhagat@monarch360.com.au>
2026-05-14 18:31:15 +0530

    fix main.py

--- app/main.py
+++ app/main.py
@@ -39,3 +39,12 @@ async def health():
 
 import logging
 logging.basicConfig(level=logging.DEBUG)
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(
+        "app.main:app",
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 8000)),
+    )
